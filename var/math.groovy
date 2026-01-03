6785c1e6093ec9e2f6a2b579fb75aed003a8c503
Malleswari232 <malleswariemani9@gmail.com>
2026-01-03 22:03:37 +0530

    Create math.groovy

---
+++ var/math.groovy
@@ -0,0 +1,22 @@
+def sum (a,b)
+{
+  echo "The Sum of ${a} and ${b} is ${a+b}"
+}
+
+
+def sub(a,b)
+{
+  echo "The Subtraction of $(a) and $(b) is $(a-b)"
+}
+
+
+def mul(a,b)
+{
+  echo "The product of $(a) and $(b) is $(a*b)"
+}
+
+
+def div(a,b)
+{
+  echo "The division of $(a) and $(b) is $(a/b)"
+}
